931bf98a39874c69d4ce272771c2ae862d4d56ff
AlazarG19 <alazargetachew70@gmail.com>
2022-10-07 22:35:36 -0700

    first commit

---
+++ script/navbar.js
@@ -0,0 +1,20 @@
+window.addEventListener("DOMContentLoaded",(event)=>{
+    let home = document.querySelector(".home-link")
+    let about = document.querySelector('.about-link')
+    let menu = document.querySelector('.menu-link')
+    let location = document.querySelector('.location-link')
+    console.log(home)
+    home.addEventListener("click",()=>{
+        window.location = "index.html"
+    })
+    about.addEventListener("click",()=>{
+        window.location = "about.html"
+    })
+    menu.addEventListener("click",()=>{
+        window.location = "menu.html"
+    })
+    location.addEventListener("click",()=>{
+        window.location = "location.html"
+    })
+    
+})
